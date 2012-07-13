f2415f4285153ccc5fcc4047716102f1a8c1c71e
kats <kfukui0621@gmail.com>
2012-07-13 23:55:35 +0900

    change some UI

---
+++ client/iphone/www/js/colosseo.js
@@ -0,0 +1,68 @@
+//
+// login
+//
+function login() {
+}
+
+//
+// logout
+//
+function logout() {
+    
+}
+
+//
+// show battle info list
+//
+function show_battle_info_list() {
+    
+}
+
+//
+// show battle status
+//
+function show_battle_status() {
+    
+}
+
+//
+// show input result
+//
+function show_status_input() {
+    
+}
+
+//
+// input result
+//
+function input_status() {
+    
+}
+
+//
+// show battle result
+//
+function show_battle_result() {
+    
+}
+
+//
+// create battle
+//
+function create_battle() {
+    
+}
+
+//
+// show friends
+//
+function show_friends() {
+
+}
+
+//
+// select participants
+//
+function select_participants() {
+    
+}
