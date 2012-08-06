995ade0a55f1cbece5c540957768f0630fd14825
hokada <hitoshi.dasu@gmail.com>
2012-08-06 21:42:34 +0900

    Fixed transition to main page

---
+++ client/android/src/com/colosseo/Main.java
@@ -0,0 +1,26 @@
+package com.colosseo;
+
+import org.apache.cordova.DroidGap;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Main extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		startActivityForResult(new Intent(this, FacebookAuth.class), 0);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+		
+		if (intent == null) 
+			return;
+		super.loadUrl("file:///android_asset/www/main.html");
+	}
+}
+
